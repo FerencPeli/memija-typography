7b764c3c4b65fb6035b0f7c85daef1fffa85c36b
ferenc.peli <ferenc.peli@accenture.com>
2018-08-27 23:48:15 +0200

    Change css dependency imports

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,57 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-var copyWebpackPluginInstance = new CopyWebpackPlugin([
-	{
-		from: './node_modules/font-awesome/',
-		to: './node_modules/font-awesome/'
-	},
-	{
-		from: './node_modules/bootstrap/',
-		to: './node_modules/bootstrap/'
-	}
-]);
-
 var htmlWebPackPluginInstance = new HtmlWebPackPlugin({
-	filename: "./index.html",
-	minify: {
-		collapseWhitespace: true,
-		html5: true,
-		minifyCSS: true,
-		removeAttributeQuotes: true,
-		removeComments: true,
-		removeEmptyAttributes: true,
-	},
-	template: "./src/index.html"
+    filename: "./index.html",
+    minify: {
+        collapseWhitespace: true,
+        html5: true,
+        minifyCSS: true,
+        removeAttributeQuotes: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+    },
+    template: "./src/index.html"
 });
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader"
-				}
-			},
-			{
-				test: /\.less$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: "style-loader"
-					},
-					{
-						loader: "css-loader"
-					},
-					{
-						loader: "less-loader"
-					}
-				]
-			},
-			{
-				test: /\.(png|jpg|css)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[path][name].[ext]'
-						}
-					}
-				]
-			}
-		]
-	},
-	plugins: [
-		copyWebpackPluginInstance,
-		htmlWebPackPluginInstance
-	]
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.less$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "less-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(css|eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        htmlWebPackPluginInstance
+    ]
 };
